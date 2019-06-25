4c0aab9f08acb2a5573d3fe0b23f0ff2e9c988d4
MaksimGarmash <president.kremlin.ru@gmail.com>
2019-06-25 18:57:16 +0300

    only user_account table was added in db

--- dao-jdbc-impl/src/main/java/com/itacademy/jd2/yi/cms/jdbc/impl/entity/UserAccount.java
+++ dao-jdbc-impl/src/main/java/com/itacademy/jd2/yi/cms/jdbc/impl/entity/UserAccount.java
@@ -18,18 +18,18 @@ public class UserAccount extends BaseEntity implements IUserAccount {
 	
 	
 
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
+//	public String getEmail() {
+//		return email;
+//	}
+//	public void setEmail(String email) {
+//		this.email = email;
+//	}
+//	public String getPassword() {
+//		return password;
+//	}
+//	public void setPassword(String password) {
+//		this.password = password;
+//	}
 	public String getRole() {
 		return role;
 	}
